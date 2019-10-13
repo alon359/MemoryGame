4598deb03457840032fc452f5540dd64acfc2d8e
Alon Davidi <alon359@gmail.com>
2019-10-13 12:05:31 +0300

    new

--- app.js
+++ app.js
@@ -1,29 +1,55 @@
 let cards = document.querySelectorAll('.memory-card');
 let hasFlippedCard = false;
 let firstCard, secondCard;
+let lock = false;
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
 function flipCard() {
+    if (lock) return;
+    if (this === firstCard) return;
     this.classList.add('flip');
 
     if (hasFlippedCard === false) {
         hasFlippedCard = true;
         firstCard = this;
-    } else {
+        return
+    }
         hasFlippedCard = false;
         secondCard = this;
-
-        console.log(firstCard.dataset.framwork);
-
-        //After 2 cards choosen
-        if (firstCard.dataset.framwork ===
-            secondCard.dataset.framwork) {
-            firstCard.removeEventListener('')
-        }
-
+        //After 2 cards chosen
+      checkForMatch();
+    }
+    
+    function disableCard() {
+        firstCard.removeEventListener('click', flipCard);
+        secondCard.removeEventListener('click', flipCard);
     }
 
+    function unFlipCard() {
+        lock = true;
+        setTimeout(() => {
+            firstCard.classList.remove('flip');
+            secondCard.classList.remove('flip');
+            reset();
+        },1100)
+    }
+    
+    function checkForMatch() {
+        let isMuch = firstCard.dataset.framwork === secondCard.dataset.framwork;
 
+        isMuch ? disableCard() : unFlipCard();
+    }
+    
+    function reset() {
+        [hasFlippedCard, lock] = [false, false];
+        [firstCard, secondCard] = [null, null];
+    }
+    
+    (function shuffle() {
+        cards.forEach(card => {
+            let randomPos = Math.floor(Math.random()*12);
+            card.style.order = randomPos.toString();
+        })
+    })();
 
-}
