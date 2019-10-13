dc75cf717940f2b1fc3bab5aed3a7d4a30d0d794
Alon Davidi <alon359@gmail.com>
2019-10-13 09:19:38 +0300

    update

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 let cards = document.querySelectorAll('.memory-card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
 function flipCard() {
-    this.classList.toggle('flip');
-    console.log(this);
+    this.classList.add('flip');
+
+    if (hasFlippedCard === false) {
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        hasFlippedCard = false;
+        secondCard = this;
+
+        console.log(firstCard.dataset.framwork);
+
+        //After 2 cards choosen
+        if (firstCard.dataset.framwork ===
+            secondCard.dataset.framwork) {
+            firstCard.removeEventListener('')
+        }
+
+    }
+
 
 
 }
